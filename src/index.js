ffbc5af2c54ba3b74b02ed7cab7e2aa2ce5bd8d5
Craig Harvie <craig.harvie@bbc.co.uk>
2020-08-11 14:07:16 +0100

    Remove console log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import statsPlugin from './stats';
 
 export default function stats(emitter, upstreamName) {
-  console.log('stats.index.js');
   return statsPlugin.bind(this, emitter, upstreamName);
 }
